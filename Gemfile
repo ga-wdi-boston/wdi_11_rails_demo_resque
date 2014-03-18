7bd00fe3657d8db2b1dfd1d28643f111c493c7c6
David Fisher <tibbon@gmail.com>
2014-03-17 21:42:18 -0400

    Basic project

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+# Our standard gemset.
+gem 'rails', '4.0.3'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'pry-rails'
+gem 'dotenv-rails'
+
+# Needed for Resque
+
+gem 'redis'
+gem 'resque', '~> 2.0.0.pre.1', github: 'resque/resque'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueDemo::Application.config.secret_key_base = '229db86b5bce163773c81a51eba9997bf3fe184e8226a44aecd2c2836b1017f4a0faf18476b5b6ace878ffba491feabe5232ff4f44e94cf5176d4c835f0c4c8d'
